daf326121e937930ca63ab7700e7b10209542e29
Francielli Rodrigues <contato.franciellirodrigues@outlook.com>
2022-08-16 22:10:44 -0300

    post method

--- src/server.js
+++ src/server.js
@@ -1,15 +1,24 @@
 const express = require("express"); /*Importou o express*/ 
 
 const app = express(); /*Inicializou o express*/ 
+app.use(express.json());
 
+/*Rout Params
 app.get("/message/:id/:user", (request, response) => {
     const { id, user } = request.params;
 
   response.send(`
   Mensagem ID: ${id}.
-  Para o usuário: ${user}.
+  Nome do usuário: ${user}.
   `);
-});
+});*/
+
+/*Query Params*/
+app.post("/user", (request, response) => {
+  const { name, email, password } = request.body;
+
+  response.json({ name, email, password });
+})
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
