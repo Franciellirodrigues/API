5609c879a45f9639c21af55239a54eb21c40fc70
Francielli Rodrigues <contato.franciellirodrigues@outlook.com>
2022-08-20 12:04:35 -0300

    adding routes

--- src/server.js
+++ src/server.js
@@ -1,24 +1,11 @@
 const express = require("express"); /*Importou o express*/ 
 
+const routes = require("./routes");
+
 const app = express(); /*Inicializou o express*/ 
 app.use(express.json());
 
-/*Rout Params
-app.get("/message/:id/:user", (request, response) => {
-    const { id, user } = request.params;
-
-  response.send(`
-  Mensagem ID: ${id}.
-  Nome do usuário: ${user}.
-  `);
-});*/
-
-/*Query Params*/
-app.post("/user", (request, response) => {
-  const { name, email, password } = request.body;
-
-  response.json({ name, email, password });
-})
+app.use(routes);
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
